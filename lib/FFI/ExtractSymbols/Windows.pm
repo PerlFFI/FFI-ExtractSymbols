c5fabcc5bd7dec40d853911bc140ead9e69f0845
Sanko Robinson <sanko@cpan.org>
2021-09-21 12:39:00 -0400

    Cache sections table locally

--- lib/FFI/ExtractSymbols/Windows.pm
+++ lib/FFI/ExtractSymbols/Windows.pm
@@ -33,8 +33,6 @@ return 1 if FFI::ExtractSymbols->can('extract_symbols') || $^O !~ /^(MSWin32|cyg
 $FFI::ExtractSymbols::mode = 'mixed';
 
 #
-my %sections;
-
 *FFI::ExtractSymbols::extract_symbols = sub
 {
   my($libpath, %callbacks) = @_;
@@ -60,22 +58,20 @@ my %sections;
   my $imageBase = $pe32plus ? unpack 'Q', substr $opt_header, 24, 8 : unpack 'V',
     substr $opt_header, 28, 4;
   my $numberOfRVAandSizes = unpack 'V', substr $opt_header, ( $pe32plus ? 108 : 112 ), 4;
-  {
-    %sections = ();
-    my $sec_begin = $peo + 24 + $sizeOfOptionalHeader;
-    my $sec_data  = substr $raw, $sec_begin, $numberOfSections * 40;
-    for my $x ( 0 .. $numberOfSections - 1 ) {
-      my $sec_head = $sec_begin + ( $x * 40 );
-      my $sec_name = unpack 'Z*', substr $raw, $sec_head, 8;
-      $sections{$sec_name} = [ unpack 'VV VVVV vv V', substr $raw, $sec_head + 8 ];
-    }
+  my $sections; # local cache
+  my $sec_begin = $peo + 24 + $sizeOfOptionalHeader;
+  my $sec_data  = substr $raw, $sec_begin, $numberOfSections * 40;
+  for my $x ( 0 .. $numberOfSections - 1 ) {
+    my $sec_head = $sec_begin + ( $x * 40 );
+    my $sec_name = unpack 'Z*', substr $raw, $sec_head, 8;
+    $sections->{$sec_name} = [ unpack 'VV VVVV vv V', substr $raw, $sec_head + 8 ];
   }
 
   # dig into directory
   my ( $edata_pos, $edata_len ) = unpack 'VV', substr $opt_header, $pe32plus ? 112 : 96, 8;
-  my @fields = unpack 'V10', substr $raw, _rva2offset($edata_pos), 40;
-  my ( $ptr_func, $ptr_name, $ptr_ord ) = map { _rva2offset( $fields[$_] ) } 7 .. 9;
-  my %retval = ( name => unpack 'Z*', substr $raw, _rva2offset( $fields[3] ), 256 );
+  my @fields = unpack 'V10', substr $raw, _rva2offset($edata_pos, $sections), 40;
+  my ( $ptr_func, $ptr_name, $ptr_ord ) = map { _rva2offset( $fields[$_], $sections ) } 7 .. 9;
+  my %retval = ( name => unpack 'Z*', substr $raw, _rva2offset( $fields[3], $sections ), 256 );
   my @ord    = unpack 'V' x $fields[5], substr $raw, $ptr_func, 4 * $fields[5];
 
   for my $idx ( 0 .. $fields[5] ) {
@@ -83,7 +79,7 @@ my %sections;
     my $func_cur = $ord[$ord_cur];    # Match the ordinal to the function RVA
     next if $idx > ( $fields[6] - 1 );
     my $name_cur = unpack 'V',  substr $raw, $ptr_name + ( 4 * $idx ), 4;
-    my $symbol = unpack 'Z*', substr $raw, _rva2offset($name_cur), 512;
+    my $symbol = unpack 'Z*', substr $raw, _rva2offset($name_cur, $sections), 512;
     $ord_cur += $fields[4];           # Add the ordinal base value
     $callbacks{export}->($symbol, $symbol);
     $callbacks{code}  ->($symbol, $symbol);
@@ -92,8 +88,8 @@ my %sections;
 
 sub _rva2offset
 {
-  my ($virtual) = @_;
-  for my $section ( values %sections ) {
+  my ($virtual, $sections) = @_;
+  for my $section ( values %$sections ) {
     if ( ( $virtual >= $section->[1] ) and ( $virtual < $section->[1] + $section->[0] ) ) {
       return $virtual - ( $section->[1] - $section->[3] );
     }
